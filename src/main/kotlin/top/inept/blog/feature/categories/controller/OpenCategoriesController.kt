d7ff0b49ea49873caa638d86e522c22353b63949
ineptchan <150328571+ineptchan@users.noreply.github.com>
2025-08-01 19:17:55 +0800

    fix(Categories): 忘记修改公开的接口了

--- src/main/kotlin/top/inept/blog/feature/categories/controller/OpenCategoriesController.kt
+++ src/main/kotlin/top/inept/blog/feature/categories/controller/OpenCategoriesController.kt
@@ -2,12 +2,17 @@ package top.inept.blog.feature.categories.controller
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import jakarta.validation.Valid
 import org.springframework.validation.annotation.Validated
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import top.inept.blog.base.ApiResponse
+import top.inept.blog.base.PageResponse
+import top.inept.blog.extensions.toApiResponse
+import top.inept.blog.extensions.toPageResponse
 import top.inept.blog.feature.categories.pojo.convert.toCategoriesVO
+import top.inept.blog.feature.categories.pojo.dto.CategoriesQueryDTO
 import top.inept.blog.feature.categories.pojo.vo.CategoriesVO
 import top.inept.blog.feature.categories.service.CategoriesService
 
@@ -20,7 +25,10 @@ class OpenCategoriesController(
 ) {
     @Operation(summary = "获取分类列表")
     @GetMapping
-    fun getCategories(): ApiResponse<List<CategoriesVO>> {
-        return ApiResponse.success(categoriesService.getCategories().map { it.toCategoriesVO() })
+    fun getCategories(@Valid categoriesQueryDTO: CategoriesQueryDTO): ApiResponse<PageResponse<CategoriesVO>> {
+        return categoriesService
+            .getCategories(categoriesQueryDTO)
+            .toPageResponse { it.toCategoriesVO() }
+            .toApiResponse()
     }
 }
