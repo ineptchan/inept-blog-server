693e39fe286f9bdf36aabf61d95be2521e65bea1
ineptchan <150328571+ineptchan@users.noreply.github.com>
2025-08-02 20:35:28 +0800

    perf(Base): 更好的BaseQueryDTO

--- src/main/kotlin/top/inept/blog/base/BaseQueryDTO.kt
+++ src/main/kotlin/top/inept/blog/base/BaseQueryDTO.kt
@@ -1,12 +1,15 @@
 package top.inept.blog.base
 
+import io.swagger.v3.oas.annotations.media.Schema
 import jakarta.validation.constraints.Positive
 import org.hibernate.validator.constraints.Range
 
 open class BaseQueryDTO(
-    @field:Positive
+    @Schema(description = "openapi.query.page")
+    @field:Positive(message = "valid.common.query.page")
     val page: Int = 1,
 
-    @field:Range(min = 1, max = 100)
+    @Schema(description = "openapi.query.size")
+    @field:Range(min = 1, max = 100, message = "valid.common.query.size")
     val size: Int = 30
 )
