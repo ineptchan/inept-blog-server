4d6ba5163e37699bcf51d888efe580e41befbaa9
ineptchan <ineptchan@outlook.com>
2026-01-17 06:31:23 +0800

    fix(tag): 删除冗余代码

--- src/main/kotlin/top/inept/blog/feature/tag/model/convert/TagConvert.kt
+++ src/main/kotlin/top/inept/blog/feature/tag/model/convert/TagConvert.kt
@@ -1,17 +1,10 @@
 package top.inept.blog.feature.tag.model.convert
 
 import top.inept.blog.feature.tag.model.dto.CreateTagDTO
-import top.inept.blog.feature.tag.model.dto.UpdateTagDTO
 import top.inept.blog.feature.tag.model.entity.Tag
 import top.inept.blog.feature.tag.model.vo.TagVO
 
 fun Tag.toTagVO() = TagVO(
-    id = this.id,
-    name = this.name,
-    slug = this.slug,
-)
-
-fun UpdateTagDTO.toTag() = Tag(
     id = this.id,
     name = this.name,
     slug = this.slug,
