3d2698a10552fc1b04a79cc5255c7ba685c931d3
ineptchan <ineptchan@outlook.com>
2026-01-17 06:31:15 +0800

    fix(categories): 删除冗余代码

--- src/main/kotlin/top/inept/blog/feature/categories/model/convert/CategoriesConvert.kt
+++ src/main/kotlin/top/inept/blog/feature/categories/model/convert/CategoriesConvert.kt
@@ -1,17 +1,10 @@
 package top.inept.blog.feature.categories.model.convert
 
-import top.inept.blog.feature.categories.model.dto.UpdateCategoriesDTO
 import top.inept.blog.feature.categories.model.dto.CreateCategoriesDTO
 import top.inept.blog.feature.categories.model.entity.Categories
 import top.inept.blog.feature.categories.model.vo.CategoriesVO
 
 fun Categories.toCategoriesVO() = CategoriesVO(
-    id = this.id,
-    name = this.name,
-    slug = this.slug
-)
-
-fun UpdateCategoriesDTO.toCategories() = Categories(
     id = this.id,
     name = this.name,
     slug = this.slug
