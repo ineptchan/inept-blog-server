6042417aaea0e2993e62740f723861a67775d16d
ineptchan <ineptchan@outlook.com>
2026-03-02 09:33:56 +0800

    fix(comment): 忘记提交更新后的评论controller

--- src/main/kotlin/top/inept/blog/feature/comment/controller/UserCommentController.kt
+++ src/main/kotlin/top/inept/blog/feature/comment/controller/UserCommentController.kt
@@ -1,16 +1,14 @@
 package top.inept.blog.feature.comment.controller
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.Parameter
 import io.swagger.v3.oas.annotations.security.SecurityRequirement
 import io.swagger.v3.oas.annotations.tags.Tag
 import jakarta.validation.Valid
 import org.springframework.http.ResponseEntity
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 import top.inept.blog.feature.comment.model.dto.CreateCommentDTO
 import top.inept.blog.feature.comment.model.vo.CommentVO
 import top.inept.blog.feature.comment.service.CommentService
@@ -25,8 +23,11 @@ class UserCommentController(
 ) {
     @PreAuthorize("hasAuthority('user:comment:create')")
     @Operation(summary = "创建评论")
-    @PostMapping
-    fun createComment(@Valid @RequestBody dto: CreateCommentDTO): ResponseEntity<CommentVO> {
-        return ResponseEntity.ok(commentService.createComment(dto))
+    @PostMapping("/{id}")
+    fun createComment(
+        @Parameter(description = "openapi.comment.article_id", required = true) @PathVariable id: Long,
+        @Valid @RequestBody dto: CreateCommentDTO
+    ): ResponseEntity<CommentVO> {
+        return ResponseEntity.ok(commentService.createComment(id, dto))
     }
 }
