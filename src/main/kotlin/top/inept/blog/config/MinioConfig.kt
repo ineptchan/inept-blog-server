a7558d22b74f2065b94e7b38a854c9dc9f14b855
ineptchan <ineptchan@outlook.com>
2026-05-29 15:46:30 +0800

    feat(object-storage): 自动创建桶并设置访问策略

--- src/main/kotlin/top/inept/blog/config/MinioConfig.kt
+++ src/main/kotlin/top/inept/blog/config/MinioConfig.kt
@@ -1,10 +1,12 @@
 package top.inept.blog.config
 
 import io.minio.BucketExistsArgs
+import io.minio.MakeBucketArgs
 import io.minio.MinioClient
+import io.minio.SetBucketPolicyArgs
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import top.inept.blog.exception.SystemException
+import top.inept.blog.extensions.log
 import top.inept.blog.properties.MinioProperties
 
 @Configuration
@@ -25,7 +27,37 @@ class MinioConfig {
         val mc = builder.build()
 
         if (!mc.bucketExists(BucketExistsArgs.builder().bucket(mp.bucket).build())) {
-            throw SystemException("未创建桶")
+            log.info("未创建桶")
+            mc.makeBucket(MakeBucketArgs.builder().bucket(mp.bucket).build())
+
+            val policy = """
+            {
+              "Version": "2012-10-17",
+              "Statement": [
+                {
+                  "Effect": "Allow",
+                  "Principal": {
+                    "AWS": ["*"]
+                  },
+                  "Action": [
+                    "s3:GetObject"
+                  ],
+                  "Resource": [
+                    "arn:aws:s3:::${mp.bucket}/*"
+                  ]
+                }
+              ]
+            }
+        """.trimIndent()
+
+            mc.setBucketPolicy(
+                SetBucketPolicyArgs.builder()
+                    .bucket(mp.bucket)
+                    .config(policy)
+                    .build()
+            )
+
+            log.info("创建桶成功")
         }
 
         return mc
